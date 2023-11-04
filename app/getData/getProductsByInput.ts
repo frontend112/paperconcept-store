7ccc23ddd4bb744daac61ac21994466c205955e3
Kamil Witek <kamilprogramuje@gmail.com>
2023-11-04 09:38:56 +0100

    error v7

--- app/getData/getProductsByInput.ts
+++ app/getData/getProductsByInput.ts
@@ -1,18 +1,20 @@
 import { getProducts as products } from "./getProducts";
 
 export const getProductsByInput = (input: string) => products.filter(({ name }) => {
+  if (input.length === 0) {
+    return false;
+  }
   const engName = replacePolishLetter(name)
   const engInput = replacePolishLetter(input)
 
-  // return engInput.every(wordI => engName.some(wordName => {
-  //   return wordI.length <= 1 ?
-  //     wordName.slice(0, wordI.length).toLocaleLowerCase()
-  //     === wordI.slice(0, wordI.length).toLocaleLowerCase()
-  //     :
-  //     wordName.slice(0, wordI.length - 1).toLocaleLowerCase()
-  //     === wordI.slice(0, wordI.length - 1).toLocaleLowerCase()
-  // }))
-  return true;
+  return engInput.every(wordI => engName.some(wordName => {
+    return wordI.length === 1 ?
+      wordName.slice(0, wordI.length).toLocaleLowerCase()
+      === wordI.slice(0, wordI.length).toLocaleLowerCase()
+      :
+      wordName.slice(0, wordI.length - 1).toLocaleLowerCase()
+      === wordI.slice(0, wordI.length - 1).toLocaleLowerCase()
+  }))
 })
 
 const replacePolishLetter = (polishWord: string) => (
