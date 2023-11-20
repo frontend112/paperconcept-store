84350bcc8d7bf97d9b6f9295e66b90fdc58a50cc
Kamil Witek <kamilprogramuje@gmail.com>
2023-11-20 19:54:44 +0100

    err v5

--- app/(routes)/(auth)/layout.tsx
+++ app/(routes)/(auth)/layout.tsx
@@ -1,14 +1,6 @@
-'use client'
 import React, { ReactNode } from 'react'
-import { useRouter } from "next/navigation";
-import { useSession } from "next-auth/react";
 
 const Layout = ({ children }: { children: ReactNode }) => {
-  const router = useRouter();
-  const { email } = useSession()?.data?.user || {};
-  if (!email) {
-    router.replace('/sign-in')
-  }
   return (
     <div>{children}</div>
   )
--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,15 +1,13 @@
-'use client'
-import { ReactNode, useState } from "react"
+import { ReactNode } from "react"
 import { Menu } from "../components/Menu/Menu"
 import { DeliveryInfo } from "../components/DeliveryInfo/DeliveryInfo"
 import { Toaster } from "@/components/ui/toaster"
 
-const Layout = ({ children }: { children: ReactNode, }) => {
-  const [_, setIsarrowhidden] = useState(false);
+const layout = ({ children }: { children: ReactNode, }) => {
   return (
     <>
       <DeliveryInfo />
-      <Menu setIsarrowhidden={setIsarrowhidden} />
+      <Menu setIsarrowhidden={() => { }} />
       <Toaster />
       <div className="mt-40">
         {children}
@@ -18,4 +16,4 @@ const Layout = ({ children }: { children: ReactNode, }) => {
   )
 }
 
-export default Layout
+export default layout
