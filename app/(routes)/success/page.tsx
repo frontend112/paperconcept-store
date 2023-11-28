aa549bbb370e9ac7466b099f429760162320b3fc
Kamil Witek <kamilprogramuje@gmail.com>
2023-11-28 18:25:01 +0100

    err v2

--- app/(routes)/success/page.tsx
+++ app/(routes)/success/page.tsx
@@ -3,9 +3,6 @@ import { clearCart } from "@/app/GlobalRedux/Features/counter/counterSlice";
 import { useDispatch } from "react-redux";
 
 const Page = () => {
-  if (localStorage.getItem("cart")) {
-    localStorage.clear();
-  }
   const dispatch = useDispatch();
   dispatch(clearCart());
   return (
