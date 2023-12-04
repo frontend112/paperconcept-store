0d0004403949f6d627135619512ba45167080d87
Kamil Witek <kamilprogramuje@gmail.com>
2023-12-04 10:03:09 +0100

    added lang to html, added prisma packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="pl">
       <body className={inter.className}>
         <ReduxProvider>{children}</ReduxProvider>
       </body>
